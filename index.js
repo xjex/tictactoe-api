5549131eba2fa222028a36cfe6f008f838a4c5bc
xjex <dev.xjmanaloto@gmail.com>
2024-07-06 03:58:31 +0800

    added cors

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/api/", logRouter);
 
 app.listen(PORT, async () => {
